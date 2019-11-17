c697582e51ebb1aefe54eb41a5e74b9af294d0a2
sakuma <sakuma@sakumanoMacBook-Pro.local>
2019-11-17 17:59:59 +0900

    update

--- src/random_graph.hpp
+++ src/random_graph.hpp
@@ -3,16 +3,6 @@
 #include "ofMain.h"
 #include <random>
 
-template <class T>
-struct Hash
-{
-	size_t operator()(T x) const noexcept
-	{
-		using type = typename underlying_type<T>::type;
-		return hash<type>{}(static_cast<type>(x));
-	}
-};
-
 class RandomGraph : public ofBaseApp
 {
 	enum class GraphType
@@ -22,19 +12,34 @@ class RandomGraph : public ofBaseApp
 		WattsStrogatz
 	};
 
+	struct Node
+	{
+		ofVec3f mPosition;
+		ofVec3f mVelocity;
+		ofVec3f mAcceleration;
+	};
+
+	struct Edge
+	{
+		int mHead;
+		int mTail;
+		float mLength;
+		float mWeight;
+	};
+
 public:
 	void setup() override;
 	void update() override;
 	void draw() override;
 	void keyPressed(int) override;
 
-	ofVec3f generateNode(float, float);
+	Node generateNode(float, float);
 	void generateErdosRenyi(int, float, float, float);
 	void generateBarabasiAlbert(int, float, float, int);
 	void generateWattsStrogatz(int, float, float, int, float);
 
-	std::vector<ofVec3f> mNodes;
-	std::vector<std::tuple<int, int>> mEdges;
+	std::vector<Node> mNodes;
+	std::vector<Edge> mEdges;
 	std::vector<ofVec2f> mVertices;
 
 	GraphType mGraphType;
@@ -58,19 +63,23 @@ void RandomGraph::setup()
 {
 	mParams = {{"largeFontSize", 20},
 			   {"smallFontSize", 10},
-			   {"edgeWidth", 0.1},
+			   {"edgeWidth", 0.01},
 			   {"nodeRadius", 0.1},
 			   {"numNodes", 100},
 			   {"radiusMean", 100.0},
-			   {"radiusStd", 20.0},
+			   {"radiusStd", 10.0},
 			   {"edgeProbMin", 0.05},
 			   {"edgeProbMax", 0.2},
 			   {"numEdgesMin", 1},
 			   {"numEdgesMax", 10},
-			   {"numNeighborsMin", 5},
+			   {"numNeighborsMin", 10},
 			   {"numNeighborsMax", 20},
-			   {"rewireProbMin", 0.005},
-			   {"rewireProbMax", 0.02},
+			   {"rewireProbMin", 0.01},
+			   {"rewireProbMax", 0.1},
+			   {"edgeWeightMin", 0.0},
+			   {"edgeWeightMax", 0.1},
+			   {"perlinNoiseNorm", 10.0},
+			   {"deltaTime", 0.1},
 			   {"cameraPositionX", 1000.0},
 			   {"cameraPositionY", 1000.0},
 			   {"cameraPositionZ", 1000.0},
@@ -79,11 +88,11 @@ void RandomGraph::setup()
 			   {"cameraTargetZ", 0.0}};
 
 	mGraphType = GraphType::WattsStrogatz;
-	mNumNeighbors = std::uniform_int_distribution<>(mParams["numNeighborsMin"], mParams["numNeighborsMax"])(mEngine);
-	mRewireProb = std::uniform_real_distribution<>(mParams["rewireProbsMin"], mParams["rewireProbMax"])(mEngine);
+	mNumNeighbors = std::uniform_int_distribution<int>(mParams["numNeighborsMin"], mParams["numNeighborsMax"])(mEngine);
+	mRewireProb = std::uniform_real_distribution<float>(mParams["rewireProbsMin"], mParams["rewireProbMax"])(mEngine);
 	generateWattsStrogatz(mParams["numNodes"], mParams["radiusMean"], mParams["radiusStd"], mNumNeighbors, mRewireProb);
 
-	ofBackground(0);
+	ofBackground(240);
 	ofEnableDepthTest();
 	ofEnableSmoothing();
 	ofSetLineWidth(mParams["edgeWidth"]);
@@ -97,17 +106,39 @@ void RandomGraph::setup()
 
 void RandomGraph::update()
 {
-	mVertices.clear();
-	for (const auto &world : mNodes)
+	for (auto &node : mNodes)
 	{
-		auto screen = mCamera.worldToScreen(world);
-		mVertices.emplace_back(screen.x, ofMap(screen.y, 0, ofGetHeight(), ofGetHeight(), 0));
+		node.mAcceleration = ofVec3f(ofSignedNoise(node.mPosition.x, node.mPosition.y, node.mPosition.z),
+									 ofSignedNoise(node.mPosition.y, node.mPosition.z, node.mPosition.x),
+									 ofSignedNoise(node.mPosition.z, node.mPosition.x, node.mPosition.y)) *
+							 mParams["perlinNoiseNorm"];
+	}
+
+	for (const auto &edge : mEdges)
+	{
+		auto direction = mNodes[edge.mTail].mPosition - mNodes[edge.mHead].mPosition;
+		auto stretch = direction - direction.getNormalized() * edge.mLength;
+		mNodes[edge.mHead].mAcceleration += edge.mWeight * stretch;
+		mNodes[edge.mTail].mAcceleration -= edge.mWeight * stretch;
+	}
+
+	for (auto &node : mNodes)
+	{
+		node.mVelocity += node.mAcceleration * mParams["deltaTime"];
+		node.mPosition += node.mVelocity * mParams["deltaTime"] + 0.5 * node.mAcceleration * mParams["deltaTime"] * mParams["deltaTime"];
+	}
+
+	mVertices.clear();
+	for (const auto &node : mNodes)
+	{
+		auto position = mCamera.worldToScreen(node.mPosition);
+		mVertices.emplace_back(position.x, ofMap(position.y, 0, ofGetHeight(), ofGetHeight(), 0));
 	}
 }
 
 void RandomGraph::draw()
 {
-	ofSetColor(255);
+	ofSetColor(0);
 	switch (mGraphType)
 	{
 	case GraphType::ErdosRenyi:
@@ -127,34 +158,36 @@ void RandomGraph::draw()
 	mSmallFont.drawString("e: Erdos Renyi", ofGetWidth() - 200, ofGetHeight() - 140);
 	mSmallFont.drawString("b: Barabasi Albert", ofGetWidth() - 200, ofGetHeight() - 120);
 	mSmallFont.drawString("w: Watts Strogatz", ofGetWidth() - 200, ofGetHeight() - 100);
+
 	mCamera.begin();
-	ofPushMatrix();
-	ofRotate(ofGetElapsedTimef() * 10);
+	ofSetColor(0);
 	for (const auto &node : mNodes)
 	{
-		ofDrawSphere(node, mParams["nodeRadius"]);
+		ofDrawSphere(node.mPosition, mParams["nodeRadius"]);
 	}
 	for (const auto &edge : mEdges)
 	{
-		ofDrawLine(mNodes[std::get<0>(edge)], mNodes[std::get<1>(edge)]);
+		ofSetColor(0, 0, 0, 255 * (1 - edge.mWeight / mParams["edgeWeightMax"]));
+		ofDrawLine(mNodes[edge.mHead].mPosition, mNodes[edge.mTail].mPosition);
 	}
-	ofPopMatrix();
 	mCamera.end();
+
 	mShader.begin();
 	mShader.setUniform2fv("vertices", &mVertices[0][0], mVertices.size());
+	ofSetColor(0);
 	ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
 	mShader.end();
 }
 
-ofVec3f RandomGraph::generateNode(float radiusMean, float radiusStd)
+RandomGraph::Node RandomGraph::generateNode(float radiusMean, float radiusStd)
 {
-	auto radius = std::normal_distribution<>(radiusMean, radiusStd)(mEngine);
-	auto theta = std::uniform_real_distribution<>(-M_PI, +M_PI)(mEngine);
-	auto phi = std::uniform_real_distribution<>(-M_PI, +M_PI)(mEngine);
+	auto radius = std::normal_distribution<float>(radiusMean, radiusStd)(mEngine);
+	auto theta = std::uniform_real_distribution<float>(-M_PI, +M_PI)(mEngine);
+	auto phi = std::uniform_real_distribution<float>(-M_PI, +M_PI)(mEngine);
 
-	return ofVec3f(radius * std::sin(theta) * std::cos(phi),
-				   radius * std::sin(theta) * std::sin(phi),
-				   radius * std::cos(theta));
+	return Node{ofVec3f(radius * std::sin(theta) * std::cos(phi),
+						radius * std::sin(theta) * std::sin(phi),
+						radius * std::cos(theta))};
 }
 
 void RandomGraph::generateErdosRenyi(int numNodes, float radiusMean, float radiusStd, float edgeProb)
@@ -172,7 +205,8 @@ void RandomGraph::generateErdosRenyi(int numNodes, float radiusMean, float radiu
 		{
 			if (std::bernoulli_distribution(edgeProb)(mEngine))
 			{
-				mEdges.emplace_back(i, j);
+				auto weight = std::uniform_real_distribution<float>(mParams["edgeWeightMin"], mParams["edgeWeightMax"])(mEngine);
+				mEdges.emplace_back(Edge{i, j, mNodes[i].mPosition.distance(mNodes[j].mPosition), weight});
 			}
 		}
 	}
@@ -187,16 +221,18 @@ void RandomGraph::generateBarabasiAlbert(int numNodes, float radiusMean, float r
 		std::vector<int> degrees(mNodes.size(), 0);
 		for (const auto &edge : mEdges)
 		{
-			++degrees[std::get<0>(edge)];
-			++degrees[std::get<1>(edge)];
-		}
-
-		for (auto j = 0; j < numEdges; ++j)
-		{
-			mEdges.emplace_back(i, std::discrete_distribution<>(degrees.begin(), degrees.end())(mEngine));
+			++degrees[edge.mHead];
+			++degrees[edge.mTail];
 		}
 
 		mNodes.emplace_back(generateNode(radiusMean, radiusStd));
+
+		for (auto j = 0; j < numEdges; ++j)
+		{
+			auto k = std::discrete_distribution<int>(degrees.begin(), degrees.end())(mEngine);
+			auto weight = std::uniform_real_distribution<float>(mParams["edgeWeightMin"], mParams["edgeWeightMax"])(mEngine);
+			mEdges.emplace_back(Edge{i, k, mNodes[i].mPosition.distance(mNodes[k].mPosition), weight});
+		}
 	}
 }
 
@@ -211,23 +247,23 @@ void RandomGraph::generateWattsStrogatz(int numNodes, float radiusMean, float ra
 	mEdges.clear();
 	for (auto i = 0; i < numNodes; ++i)
 	{
-		std::vector<std::tuple<int, float>> norms;
+		std::vector<std::pair<float, int>> norms;
 		for (auto j = 0; j < numNodes; ++j)
 		{
-			norms.emplace_back(j, mNodes[j].distance(mNodes[i]));
+			norms.emplace_back(mNodes[j].mPosition.distance(mNodes[i].mPosition), j);
 		}
-		std::sort(norms.begin(), norms.end(), [](const std::tuple<int, float> &norm1, const std::tuple<int, float> &norm2) {
-			return std::get<1>(norm1) < std::get<1>(norm2);
-		});
+		std::sort(norms.begin(), norms.end());
 		for (auto j = 0; j < numNeighbors; ++j)
 		{
+			auto weight = std::uniform_real_distribution<float>(mParams["edgeWeightMin"], mParams["edgeWeightMax"])(mEngine);
 			if (std::bernoulli_distribution(rewireProb)(mEngine))
 			{
-				mEdges.emplace_back(i, std::uniform_int_distribution<>(0, numNodes - 1)(mEngine));
+				auto k = std::uniform_int_distribution<int>(0, numNodes - 1)(mEngine);
+				mEdges.emplace_back(Edge{i, k, mNodes[i].mPosition.distance(mNodes[k].mPosition), weight});
 			}
 			else
 			{
-				mEdges.emplace_back(i, std::get<0>(norms[j]));
+				mEdges.emplace_back(Edge{i, norms[j].second, mNodes[i].mPosition.distance(mNodes[norms[j].second].mPosition), weight});
 			}
 		}
 	}
@@ -240,22 +276,22 @@ void RandomGraph::keyPressed(int key)
 	case 'e':
 	{
 		mGraphType = GraphType::ErdosRenyi;
-		mEdgeProb = std::uniform_real_distribution<>(mParams["edgeProbMin"], mParams["edgeProbMax"])(mEngine);
+		mEdgeProb = std::uniform_real_distribution<float>(mParams["edgeProbMin"], mParams["edgeProbMax"])(mEngine);
 		generateErdosRenyi(mParams["numNodes"], mParams["radiusMean"], mParams["radiusStd"], mEdgeProb);
 	}
 	break;
 	case 'b':
 	{
 		mGraphType = GraphType::BarabasiAlbert;
-		mNumEdges = std::uniform_int_distribution<>(mParams["numEdgesMin"], mParams["numEdgesMax"])(mEngine);
+		mNumEdges = std::uniform_int_distribution<int>(mParams["numEdgesMin"], mParams["numEdgesMax"])(mEngine);
 		generateBarabasiAlbert(mParams["numNodes"], mParams["radiusMean"], mParams["radiusStd"], mNumEdges);
 	}
 	break;
 	case 'w':
 	{
 		mGraphType = GraphType::WattsStrogatz;
-		mNumNeighbors = std::uniform_int_distribution<>(mParams["numNeighborsMin"], mParams["numNeighborsMax"])(mEngine);
-		mRewireProb = std::uniform_real_distribution<>(mParams["rewireProbsMin"], mParams["rewireProbMax"])(mEngine);
+		mNumNeighbors = std::uniform_int_distribution<int>(mParams["numNeighborsMin"], mParams["numNeighborsMax"])(mEngine);
+		mRewireProb = std::uniform_real_distribution<float>(mParams["rewireProbsMin"], mParams["rewireProbMax"])(mEngine);
 		generateWattsStrogatz(mParams["numNodes"], mParams["radiusMean"], mParams["radiusStd"], mNumNeighbors, mRewireProb);
 	}
 	break;
